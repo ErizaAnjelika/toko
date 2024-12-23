42681dc0715d04cf9cd044368850318a50681570
Eriza anjelika vega <139231295+ErizaAnjelika@users.noreply.github.com>
2024-12-23 13:17:22 +0700

    create endpoint

---
+++ configs/config.go
@@ -0,0 +1,16 @@
+package configs
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() (*gorm.DB, error) {
+	db, err := gorm.Open("mysql", "root@tcp(127.0.0.1:3306)/ecomm?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		return nil, err
+	}
+	DB = db
+	return DB, nil
+}
