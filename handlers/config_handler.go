445f16894898819ee5e868808db3af7539145e83
Eriza anjelika vega <139231295+ErizaAnjelika@users.noreply.github.com>
2024-12-24 09:39:10 +0700

    create all api

---
+++ handlers/config_handler.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+"github.com/jinzhu/gorm")
+
+var db *gorm.DB
+
+func init(){
+	var err error
+	db, err=gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/ecomm?charset=utf8mb4&parseTime=True&loc=Local")
+
+	if err !=nil{
+		panic("failed to connect database")
+	}
+}
--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"toko/configs"
 	"toko/handlers"
+	"toko/middlewares"
+	"toko/migrations"
 
 	"github.com/gin-gonic/gin"
 
-	_ "github.com/go-sql-driver/mysql"
+	"net/http"
+	_ "net/http/pprof"
 )
 
 func main() {
@@ -16,14 +19,31 @@ func main() {
 	}
 	defer db.Close()
 
+	migrations.Migrate(db)
+
 	router := gin.Default()
 
 	// Rute CRUD Produk
-	router.GET("/products", handlers.ListProducts(db))
+	router.GET("/products", middlewares.AuthMiddleware(), handlers.ListProducts(db))
 	router.GET("/products/:id", handlers.GetProduct(db))
 	router.POST("/products", handlers.CreateProduct(db))
 	router.PUT("/products/:id", handlers.UpdateProduct(db))
 	router.DELETE("/products/:id", handlers.DeleteProduct(db))
+
+	// Rute CRUD Product Category
+	router.GET("/product-categories", handlers.ListProductCategories(db))
+	router.GET("/product-categories/:id", handlers.GetProductCategory(db))
+	router.POST("/product-categories", handlers.CreateProductCategory(db))
+	router.PUT("/product-categories/:id", handlers.UpdateProductCategory(db))
+	router.DELETE("/product-categories/:id", handlers.DeleteProductCategory(db))
+
+	router.POST("/transactions", handlers.CreateTransaction(db))
+	router.GET("/transactions/:id", handlers.GetTransactionWithItems(db))
+
+	router.POST("/login", handlers.Login(db))
+	router.POST("/register", handlers.Register(db))
+
+	router.GET("/debug/pprof/*pprof", gin.WrapH(http.DefaultServeMux))
 
 	router.Run(":8080")
 }
